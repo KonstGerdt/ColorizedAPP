bcb13f437ecb1eb9828281b0ec597946754fd42c
KonstGerdt <konstantinivanovich.d@gmail.com>
2023-01-22 17:06:47 +0300

    1st comm

--- ColorizedAPP/ViewController.swift
+++ ColorizedAPP/ViewController.swift
@@ -6,14 +6,53 @@
 //
 
 import UIKit
-
 class ViewController: UIViewController {
-
+    @IBOutlet weak var ColorWiev: UIView!
+    
+    @IBOutlet weak var redLabel: UILabel!
+    @IBOutlet weak var greenLabel: UILabel!
+    @IBOutlet weak var blueLabel: UILabel!
+    
+    @IBOutlet weak var redSlider: UISlider!
+    @IBOutlet weak var greenSlider: UISlider!
+    @IBOutlet weak var blueSlider: UISlider!
+    
+    
+    //MARK: - LifeCicle View
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        ColorWiev.layer.cornerRadius = 15
+        
+        redSlider.minimumTrackTintColor = .red
+        greenSlider.minimumTrackTintColor = .green
+        
+        ColorWiev.backgroundColor = UIColor(
+            red: CGFloat(redSlider.value),
+            green: CGFloat(greenSlider.value),
+            blue: CGFloat(blueSlider.value),
+            alpha: 1
+        )
+        
+        redLabel.text = String(format: "%.2f", redSlider.value )
+        greenLabel.text = String(format: "%.2f", greenSlider.value )
+        blueLabel.text = String(format: "%.2f", blueSlider.value)
+        
+        }
+//MARK: - IBaction
+    @IBAction func RGBSlider(_ sender: UISlider) {
+        ColorWiev.backgroundColor = UIColor(
+                red: CGFloat(redSlider.value),
+                green: CGFloat(greenSlider.value),
+                blue: CGFloat(blueSlider.value),
+                alpha: 1
+            )
+            
+            redLabel.text = String(format: "%.2f", redSlider.value)
+            greenLabel.text = String(format: "%.2f", greenSlider.value)
+            blueLabel.text = String(format: "%.2f", blueSlider.value)
+        
     }
-
+    
 
 }
 
